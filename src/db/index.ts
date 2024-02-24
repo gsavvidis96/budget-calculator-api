b6d3746924d044cab3b0a55cab9f7cb218a956bb
Giannis Savvidis <giannis.savvidis@slashdata.co>
2024-02-24 12:34:47 +0200

    budgets-getAll pagination

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,7 @@ import ws from "ws";
 neonConfig.webSocketConstructor = ws;
 types.setTypeParser(types.builtins.NUMERIC, (value) => parseFloat(value)); // return numeric as float
 types.setTypeParser(types.builtins.TIMESTAMP, (value) => value.toString()); // return dates as strings
+types.setTypeParser(types.builtins.INT8, (value) => parseInt(value)); // return int8 as js int (for count queries)
 
 export const getDb = () => {
   const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
--- src/functions/budgets/handlers/getAll.ts
+++ src/functions/budgets/handlers/getAll.ts
@@ -11,29 +11,53 @@ export const handler = async (
 
   try {
     const decodedUser = await authenticate(event.headers);
+    const limit = !isNaN(Number(event?.queryStringParameters?.limit))
+      ? Number(event?.queryStringParameters?.limit)
+      : 10;
+    const offset = !isNaN(Number(event?.queryStringParameters?.offset))
+      ? Number(event?.queryStringParameters?.offset)
+      : 0;
 
-    const budgets = await db
-      .selectFrom("budgets")
-      .where("user_id", "=", decodedUser.id)
-      .selectAll()
-      .select((eb) =>
-        eb
-          .selectFrom("budget_items")
-          .whereRef("budget_items.budget_id", "=", "budgets.id")
-          .select(() =>
-            sql`
-        COALESCE(SUM(CASE WHEN "type" = 'INCOME' THEN "value" ELSE 0 END), 0) -
-        COALESCE(SUM(CASE WHEN "type" = 'EXPENSES' THEN "value" ELSE 0 END), 0)`.as(
-              "balance"
-            )
+    const response = await db.transaction().execute(async (transaction) => {
+      const [budgets, countResponse] = await Promise.all([
+        transaction
+          .selectFrom("budgets")
+          .where("user_id", "=", decodedUser.id)
+          .selectAll()
+          .select((eb) =>
+            eb
+              .selectFrom("budget_items")
+              .whereRef("budget_items.budget_id", "=", "budgets.id")
+              .select(() =>
+                sql`
+          COALESCE(SUM(CASE WHEN "type" = 'INCOME' THEN "value" ELSE 0 END), 0) -
+          COALESCE(SUM(CASE WHEN "type" = 'EXPENSES' THEN "value" ELSE 0 END), 0)`.as(
+                  "balance"
+                )
+              )
+              .as("balance")
           )
-          .as("balance")
-      )
-      .execute();
+          .limit(limit)
+          .offset(offset)
+          .execute(),
+        transaction
+          .selectFrom("budgets")
+          .where("user_id", "=", decodedUser.id)
+          .select((eb) => eb.fn.countAll().as("count"))
+          .executeTakeFirst(),
+      ]);
+
+      return {
+        budgets,
+        total_count: countResponse?.count,
+        page_size: limit,
+        page_number: offset,
+      };
+    });
 
     return {
       statusCode: 200,
-      body: JSON.stringify(budgets),
+      body: JSON.stringify(response),
     };
   } catch (e: any) {
     return handleError(e);
